1ecd4ee520d2f87e215b5bf63808e54e417442cc
permlap-phola <permlap@tatugacamp.com>
2024-08-20 14:34:14 +0700

    Setup project (#1)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Anuphan: ["Anuphan", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
