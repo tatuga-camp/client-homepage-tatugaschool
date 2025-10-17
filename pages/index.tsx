af391296701c3543d8923faadbefadf3999700df
permlap-phola <permlap@tatugacamp.com>
2025-10-18 00:50:44 +0700

    Update Number Info

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,15 +104,15 @@ export default function Home() {
             your students and your school.
           </p>
           <section className="flex col-span-1 flex-col items-center justify-center gap-1">
-            <h1 className="text-4xl font-bold">1,356+</h1>
+            <h1 className="text-4xl font-bold">2,136+</h1>
             <span>Number of School</span>
           </section>
           <section className="flex col-span-1 flex-col items-center justify-center gap-1">
-            <h1 className="text-4xl font-bold">2,670+</h1>
+            <h1 className="text-4xl font-bold">3,793+</h1>
             <span>Number of User</span>
           </section>
           <section className="flex col-span-1 flex-col items-center justify-center gap-1">
-            <h1 className="text-4xl font-bold">34,155+</h1>
+            <h1 className="text-4xl font-bold">57,268+</h1>
             <span>Number of Student</span>
           </section>
         </nav>
